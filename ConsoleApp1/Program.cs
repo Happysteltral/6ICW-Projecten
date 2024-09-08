59be7fcb1bd1017beab4baff8b2fc66573eb6ca9
Nicky Vergauwen <nickyvergauwen@gmail.com>
2024-09-08 21:00:22 +0200

    tweede project

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hallo, World!");
+Console.Beep(264, 1000);
+Console.Beep(297, 1000);
+Console.Beep(330, 1000);
+Console.Beep(352, 1000);
+Console.Beep(396, 1000);
+Console.Beep(440, 1000);
+Console.Beep(495, 1000);
+Console.Beep(528, 1000);
