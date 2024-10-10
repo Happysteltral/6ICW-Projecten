2058eb5eae95b06147ab2fc74abb5bf569e38ab1
Nicky Vergauwen <nickyvergauwen@gmail.com>
2024-10-10 21:08:42 +0200

    Pong

---
+++ pong/pong/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       bool gameOver=false;
+            while (!gameOver)
+            {
+                //Input verwerken
+ 
+                //Update game 
+
+                
+                //Render (Console.Write/Console.WriteLine)
+
+               
+                //Wait
+                System.Threading.Thread.Sleep(50);
+                
+                Console.Clear();
+       
+
+            }    
+
+    }
+}
